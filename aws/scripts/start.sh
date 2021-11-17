f03f2f8ddd569235cf204ade2eef029208f11b7c
Edward Lynch-Milner <eddylynch9@gmail.com>
2021-11-17 22:07:01 +0000

    Attempt #2 at fixing build failure

--- aws/scripts/start.sh
+++ aws/scripts/start.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
 
+cd /home/ec2-user/route-planning/source
+
 # This script is responsible for (re)starting the service after an install
-aws/scripts/
+aws/scripts/route-planning.sh restart
