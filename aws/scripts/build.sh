8144cce3c635e0ce9346dcf0387ddac103fbcd21
Edward Lynch-Milner <eddylynch9@gmail.com>
2021-11-18 07:59:51 +0000

    Updated build script

--- aws/scripts/build.sh
+++ aws/scripts/build.sh
@@ -35,6 +35,14 @@ mvn_exit="$?"
 echo "Installing service file"
 aws/scripts/install-service.sh
 
+echo "Backing up revision to AWS S3"
+current_date=$(date +"%m_%d_%Y_%H_%M_%S")
+backup_dir="route-planning_$current_date"
+cd ../..
+cp -r route-planning "$backup_dir"
+aws s3 cp --recursive "$backup_dir" s3://route-planning-bucket/revisions
+rm -rf "$backup_dir"
+
 if [ "$mvn_exit" -ne "0" ]; then
 	echo "Build failed, see $LOG"
 	exit 1
